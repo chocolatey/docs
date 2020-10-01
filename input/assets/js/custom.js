b1e0ef860c97c5365cef5695086bf48af9ea112a
Stephanie Burrows <designxsteph@gmail.com>
2020-10-01 11:35:12 -0500

    (GH-2) Remove Mermaid JS/CSS
    We are not using/need Mermaid.js in our documentation at this time so
    it has been removed.

--- input/assets/js/custom.js
+++ input/assets/js/custom.js
@@ -43,50 +43,6 @@ function fallbackMessage(action) {
     return actionMsg;
 }
 
-// Mermaid
-$(document).ready(function() {
-    mermaid.initialize(
-    {
-        flowchart:
-        {
-            useMaxWidth: false
-        },
-        startOnLoad: false,
-        cloneCssStyles: false
-    });
-    mermaid.init(undefined, ".mermaid");
-
-    // Remove the max-width setting that Mermaid sets
-    var mermaidSvg = $('.mermaid svg');
-    mermaidSvg.addClass('img-responsive');
-    mermaidSvg.css('max-width', '');
-
-    // Make it scrollable
-    var target = document.querySelector(".mermaid svg");
-    if(target !== null)
-    {
-        var panZoom = window.panZoom = svgPanZoom(target, {
-            zoomEnabled: true,
-            controlIconsEnabled: true,
-            fit: true,
-            center: true,
-            maxZoom: 20,
-            zoomScaleSensitivity: 0.6
-        });
-
-        // Do the reset once right away to fit the diagram
-        panZoom.resize();
-        panZoom.fit();
-        panZoom.center();
-
-        $(window).resize(function(){
-            panZoom.resize();
-            panZoom.fit();
-            panZoom.center();
-        });
-    }
-});
-
 // Style blockquotes with emojis
 $.each($('blockquote'), function () {
     var warningEmoji = "⚠️";
