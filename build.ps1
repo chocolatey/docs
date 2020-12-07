5efe5b96265538f4de28b34bca82bb1d2dcc79b8
Gary Ewan Park <gep13@gep13.co.uk>
2020-12-07 15:10:31 +0000

    (build) Opt out of telemetry

--- build.ps1
+++ build.ps1
@@ -1,2 +1,9 @@
+$env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE = '1'
+$env:DOTNET_CLI_TELEMETRY_OPTOUT = '1'
+$env:DOTNET_NOLOGO = '1'
+
 dotnet tool restore
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
 dotnet cake --target=Statiq-Build
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
