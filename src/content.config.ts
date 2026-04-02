91a3223a93592d2f113ba2675e1bcb24ae1be0fa
Steph Hays <steph@stephhays.dev>
2026-04-02 11:53:05 -0500

    (#1311) Upgrade Astro via choco-theme 2.7.0
    This updates choco-theme packages to 2.7.0. Included in this release is
    an upgrade to the version of Astro to 6.x. With the upgrade to Astro,
    many files needed to be updated due to breaking changes in the way v6
    works. No apparent changes should be visible to the end user, and
    adding the process of adding new content remains the same.

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,15 @@
 // 1. Import utilities from `astro:content`
-import { z, defineCollection } from 'astro:content';
+import { defineCollection } from 'astro:content';
 
-// 2. Define your collection(s)
+// 2. Import loader(s)
+import { glob } from 'astro/loaders';
+
+// 3. Import Zod
+import { z } from 'astro/zod';
+
+// 4. Define a `loader` and `schema` for each collection
 const docsCollection = defineCollection({
-    type: 'content',
+    loader: glob({ base: './src/content/docs', pattern: '**/*.{md,mdx}' }),
     schema: z.object({
         order: z.number(),
         xref: z.string(),
